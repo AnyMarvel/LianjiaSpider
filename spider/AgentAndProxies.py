e0e8dd7e78d676ef8eed7094c0b26b60232175a1
lijuntao <lijuntao1@jd.com>
2017-11-13 11:36:48 +0800

    生成动态代理服务器ip池

--- spider/AgentAndProxies.py
+++ spider/AgentAndProxies.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Some User Agents
+# Agents  And proxies
+
 import random
 import re
 import requests
@@ -23,13 +25,67 @@ hds = [{'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.
        {'User-Agent': 'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'}, \
        {'User-Agent': 'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11'}]
 
-geturl = requests.get('http://www.xicidaili.com/', headers=hds[random.randint(0, len(hds) - 1)])
 
-# partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>(.*?)</tr>', re.S)
-partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>.*?<td>(.*?)'
-                     '</td>.*?<td>(.*?)</td>.*?td>.*?<td>(.*?)<.*?</tr>', re.S)
+# geturl = requests.get('http://www.xicidaili.com/', headers=hds[random.randint(0, len(hds) - 1)])
+#
+# # partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>(.*?)</tr>', re.S)
+# partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>.*?<td>(.*?)'
+#                      '</td>.*?<td>(.*?)</td>.*?td>.*?<td>(.*?)<.*?</tr>', re.S)
+#
+# result = re.findall(partent, geturl.text)
+# infos = {}
+# for item in result:
+#     if item[2] != "socks4/5":
+#         print u"地址:" + item[0], u"端口:" + item[1], u"协议:" + item[2]
+#
 
-result = re.findall(partent, geturl.text)
-infos = {}
-for item in result:
-    print u"地址:"+item[0], u"端口:"+item[1], u"协议:"+item[2]
+class GetIpProxy():
+    def __init__(self):
+        self.infos = {}
+        self.getIpPool()
+
+    def getIpPool(self):
+        geturl = requests.get('http://www.xicidaili.com/', headers=hds[random.randint(0, len(hds) - 1)])
+        # partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>(.*?)</tr>', re.S)
+        partent = re.compile('<tr class=.*?>.*?<td class="country"><img src=.*?td>.*?<td>(.*?)'
+                             '</td>.*?<td>(.*?)</td>.*?td>.*?<td>(.*?)<.*?</tr>', re.S)
+        result = re.findall(partent, geturl.text)
+        for item in result:
+            if item[2] != "socks4/5":
+                # print u"地址:" + item[0], u"端口:" + item[1], u"协议:" + item[2]
+                proxy_utl = '{0}://{1}:{2}'.format(item[2], item[0], item[1])
+                self.infos[proxy_utl.lower()] = item[2].lower()
+
+    def judge_ip(self, proxy_url, protocol):
+        # 判断给出的代理 ip 是否可用
+        http_url = 'http://www.163.com/'
+
+        print("proxy_url", proxy_url)
+        try:
+            proxy_dict = {
+                protocol: proxy_url
+            }
+            response = requests.get(http_url, proxies=proxy_dict, timeout=2)
+
+        except Exception as e:
+            print("[没有返回]代理{0} 端口号及ip不可用".format(proxy_url))
+            return False
+        else:
+            code = response.status_code
+            if code >= 200 or code < 300:
+                print("代理  {0} 端口号及ip 可用".format(proxy_url))
+                return True
+            else:
+                print("[有返回，但是状态码异常]代理 {0} 端口号及ip不可用".format(proxy_url))
+                return False
+
+    def get_random_ip(self):
+        for i in range(len(self.infos.keys())):
+            judge_ip_status = self.judge_ip(self.infos.keys()[i], self.infos.values()[i])
+            if judge_ip_status:
+
+                return self.infos.values()[i]
+
+
+getIpProxy = GetIpProxy()
+getIpProxy.get_random_ip()
