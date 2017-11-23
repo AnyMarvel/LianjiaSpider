9f5e38ffc23335376ff8b531949fb79d6564c87a
lijuntao <lijuntao1@jd.com>
2017-11-23 23:23:26 +0800

    提交zaishou_constant

---
+++ spider/zaishou/zaishou_constant.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import sys
+
+defaultencoding = 'utf-8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
+
+class chengjiao_constant:
+    def __init__(self):
+        self.chengjiao_source_data = {}
+        self.init_source_data()
+
+    def init_source_data(self):
+        self.chengjiao_source_data[''] = 0
+        self.chengjiao_source_data[''] = 1
+        self.chengjiao_source_data[''] = 2
+        self.chengjiao_source_data[''] = 3
+        self.chengjiao_source_data[''] = 4
+        self.chengjiao_source_data[''] = 5
+        self.chengjiao_source_data[''] = 6
+        self.chengjiao_source_data[''] = 7
+        self.chengjiao_source_data[''] = 8
+        self.chengjiao_source_data[''] = 9
+        self.chengjiao_source_data[''] = 10
+        self.chengjiao_source_data[''] = 11
+        self.chengjiao_source_data[''] = 12
+        self.chengjiao_source_data[''] = 13
+        self.chengjiao_source_data[''] = 14
+        self.chengjiao_source_data[''] = 15
+        self.chengjiao_source_data[''] = 16
+        self.chengjiao_source_data[''] = 17
+        self.chengjiao_source_data[''] = 18
+        self.chengjiao_source_data[''] = 19
+        self.chengjiao_source_data[''] = 20
+        self.chengjiao_source_data[''] = 21
+        self.chengjiao_source_data[''] = 22
+        self.chengjiao_source_data[''] = 23
+        self.chengjiao_source_data[''] = 24
+        self.chengjiao_source_data[''] = 25
+        self.chengjiao_source_data[''] = 26
+        self.chengjiao_source_data[''] = 27
+        self.chengjiao_source_data[''] = 28
+        self.chengjiao_source_data[''] = 29
+        self.chengjiao_source_data[''] = 30
+        self.chengjiao_source_data[''] = 31
+        self.chengjiao_source_data[''] = 32
+        self.chengjiao_source_data[''] = 33
+        self.chengjiao_source_data[''] = 34
+        self.chengjiao_source_data[''] = 35
+        self.chengjiao_source_data[''] = 36
+        self.chengjiao_source_data[''] = 37
+        self.chengjiao_source_data[''] = 38
+        self.chengjiao_source_data[''] = 39
